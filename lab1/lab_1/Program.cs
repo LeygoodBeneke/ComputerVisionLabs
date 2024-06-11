bba887c9eabb9cef5e0e41ce0fbdf1d97f7761c9
VasiliyDenisov <mr.v.m.denisov@mail.ru>
2024-06-11 19:03:00 +0300

    hotfix

--- lab1/lab_1/Program.cs
+++ lab1/lab_1/Program.cs
@@ -15,9 +15,6 @@ namespace lab1
     internal static class Program
     {
 
-        /// <summary>
-        /// Главная точка входа для приложения.
-        /// </summary>
         [STAThread]
         static void Main()
         {
